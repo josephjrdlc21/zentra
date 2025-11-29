19f59b48d0c9af59c9d4595de1271883ff1b962b
Joseph Dela Cruz Jr <josephdelacruzjr4@gmail.com>
2025-11-29 11:49:06 +0800

    edit permission task update

--- routes/portal/task.php
+++ routes/portal/task.php
@@ -8,7 +8,7 @@ Route::middleware('portal.auth')->prefix('tasks')->name('tasks.')->group(functio
     Route::get('/board', [TaskController::class, 'board'])->name('board')->middleware('portal.permission:portal.tasks.board');
     Route::get('/create', [TaskController::class, 'create'])->name('create')->middleware('portal.permission:portal.tasks.create');
     Route::post('/store', [TaskController::class, 'store'])->name('store')->middleware('portal.permission:portal.tasks.create');
-    Route::get('/edit/{id?}', [TaskController::class, 'edit'])->name('edit')->middleware('portal.permission:portal.tasks.edit');
+    Route::get('/edit/{id?}', [TaskController::class, 'edit'])->name('edit')->middleware('portal.permission:portal.tasks.update');
     Route::put('/update/{id?}', [TaskController::class, 'update'])->name('update')->middleware('portal.permission:portal.tasks.update');
     Route::put('/update-status/{id?}', [TaskController::class, 'update_status'])->name('update_status')->middleware('portal.permission:portal.tasks.update_status');
     Route::get('/show/{id?}', [TaskController::class, 'show'])->name('show')->middleware('portal.permission:portal.tasks.view');
